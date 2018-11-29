2bc4188eef798c162a9d157e451203dc027ef1d2
Thaynara Santos <thaynarambsantos@gmail.com>
2018-11-29 13:04:27 -0200

    arquivo php

---
+++ php.php
@@ -0,0 +1,45 @@
+<?php
+
+			#tipos de dados
+
+//tipo integer, inteiro
+	//é qualquer número sem decimais, positivo ou negativo
+$ano_nascimento = 1996;
+$idade = 22;
+$qtd = 1458;
+
+//tipo float ou double, ponto flutuante
+	// são os números reais
+$dinheiro = 7.25;
+$altura = 1.59;
+$peso = 0.800;
+
+//tipo string
+	//é uma série de caracteres ex: texto [usar apóstofros; aspas simples]
+$texto = 'o php é uma linguagem server-side';
+$nome = 'thaynara';
+$code = 'RVL-0108';
+	//string com aspas duplas interpretam as variáveis
+$produto = 'pizza';
+$texto = "ele comprou uma $produto no joey";
+
+//tipo boolean, booleano
+	//valores verdadeiro ou falso
+$sim = true;
+$nao = false;
+$idade < 50;
+
+			#operadores 
+			#operações matemáticas
+
+//adição +
+
+$a = 4;
+$b = 2;
+
+//echo $a + $b;
+
+//subtração -
+echo $a - $b;
+//multiplicação *
+//divisão /
